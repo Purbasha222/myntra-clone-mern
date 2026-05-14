9ac6076785eeb9123951767e6a7c071bb4baa8aa
Purbasha Goswami <purbashagoswami2@gmail.com>
2026-05-14 10:40:09 +0530

    working back with resend

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -1,53 +1,53 @@
-import nodemailer from "nodemailer";
+// import nodemailer from "nodemailer";
 
-export const sendOTPEmail = async (toEmail, otp) => {
-  const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    connectionTimeout: 10000,
-    greetingTimeout: 10000,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-    tls: {
-      rejectUnauthorized: false,
-    },
-    socketOptions: {
-      family: 4,
-    },
-  });
-
-  try {
-    const result = await transporter.sendMail({
-      from: `<${process.env.EMAIL_USER}>`,
-      to: toEmail,
-      subject: "Your OTP Code",
-      html: `<p>Your OTP is <strong>${otp}</strong>. It is valid for <strong>10 minutes</strong>.</p>`,
-    });
-
-    console.log("Accepted:", result.accepted);
-  } catch (error) {
-    console.error("Email error:", error.message);
-  }
-};
-
-// import { Resend } from "resend";
-
-// const resend = new Resend(process.env.API_KEY);
-
-// export const sendOTPEmail = async function (toEmail, otp) {
-//   const { data, error } = await resend.emails.send({
-//     from: "Myntra <onboarding@resend.dev>",
-//     to: [toEmail],
-//     subject: "Your OTP",
-//     html: `<p>Your OTP is <strong>${otp}</strong>. Valid for 10 minutes.</p>`,
+// export const sendOTPEmail = async (toEmail, otp) => {
+//   const transporter = nodemailer.createTransport({
+//     host: "smtp.gmail.com",
+//     port: 465,
+//     secure: true,
+//     connectionTimeout: 10000,
+//     greetingTimeout: 10000,
+//     auth: {
+//       user: process.env.EMAIL_USER,
+//       pass: process.env.EMAIL_PASS,
+//     },
+//     tls: {
+//       rejectUnauthorized: false,
+//     },
+//     socketOptions: {
+//       family: 4,
+//     },
 //   });
 
-//   if (error) {
-//     return console.error({ error });
-//   }
+//   try {
+//     const result = await transporter.sendMail({
+//       from: `<${process.env.EMAIL_USER}>`,
+//       to: toEmail,
+//       subject: "Your OTP Code",
+//       html: `<p>Your OTP is <strong>${otp}</strong>. It is valid for <strong>10 minutes</strong>.</p>`,
+//     });
 
-//   console.log({ data });
+//     console.log("Accepted:", result.accepted);
+//   } catch (error) {
+//     console.error("Email error:", error.message);
+//   }
 // };
+
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.API_KEY);
+
+export const sendOTPEmail = async function (toEmail, otp) {
+  const { data, error } = await resend.emails.send({
+    from: "Myntra <onboarding@resend.dev>",
+    to: [toEmail],
+    subject: "Your OTP",
+    html: `<p>Your OTP is <strong>${otp}</strong>. Valid for 10 minutes.</p>`,
+  });
+
+  if (error) {
+    return console.error({ error });
+  }
+
+  console.log({ data });
+};
